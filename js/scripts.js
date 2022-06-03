e259c52ac8d0dfd5a3b41c25e1ec14756736d45c
Liam Campbell <liam22campbell@gmail.com>
2022-06-02 22:56:27 -0700

    WIP display paragrahs based on user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-const dob =$('#born').val();
+$(document).ready(function() {
+	$("form-id#voter").submit(function() {
+	event.preventDefault
+	
+	const vote = $("#voter").val()
+	
+	
+	if (vote === "over18") {
+		$(".voter-info").toggle();
+	} else if (vote === "under18") { 
+		$(".ineligible").show();
+	} else {(vote === "IDK") 
+		$(".ineligible").show();
+	}
+	});
+});
+
+
+
+
+
+
+
